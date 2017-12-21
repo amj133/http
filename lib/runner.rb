fd47ea9092ba2981712016067739944dc9918e5e
amj133 <amj@vt.edu>
2017-12-21 15:36:15 -0700

    same as before with runner updated and good to go

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,42 +1,21 @@
 require 'socket'
 require 'pry'
 require './lib/server'
-require './lib/response'
-require './lib/guessing_game'
 require './lib/request'
-require './lib/word_lookup'
+require './lib/response'
 
-tcp_server = TCPServer.new(9292)
 server = Server.new
+server.start_server
 request = Request.new
 response = Response.new
 
-until request.path == "/shutdown" && request.verb == "GET"
-  puts "Ready for a request"
-  client = tcp_server.accept
-  # binding.pry
-  request_lines = server.request_lines(client)
-  # binding.pry
-  request.parse(request_lines)
-  response.parse(request_lines)
-  #-----------------------------------------
-  guess = request.request_guess(client)
-  # binding.pry
-  #-----------------------------------------
-
-  response_message = response.create_message(request)
-  response_footer = response.create_response_footer
-  response_body = response.create_response_body(response_message + response_footer)
-  response_header = response.create_response_header
-
-  puts "Got this request:"
-  puts request_lines.inspect
-
-  client.puts response.header
-  client.puts response.body
-
-  puts "Sending response."
-  puts ["Wrote this response:", response.header, response.body].join("\n")
+until request.path == "/shutdown"
+  client = server.listen
+  request_lines = server.get_request_lines
+  server.parse_request(request, request_lines)
+  server.prepare_response(response, request, request_lines)
+  server.print_request(request_lines)
+  server.send_response(response)
   client.close
 end
 
